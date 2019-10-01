024b8644781cb0b85452aa0b85d94d2b607d0faa
Sunset Snell <anicia.snell.as@gmail.com>
2019-10-01 19:30:43 -0400

    finished

--- main.py
+++ main.py
@@ -5,64 +5,64 @@ import os
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route("/homepage")
+@app.route("/")
 def index():
     return render_template('index.html')
 
 @app.route("/homepage", methods=['POST', 'GET'])
 def validation():
-    valid_username = request.form['username']
-    valid_password = request.form['password']
-    valid_verify = request.form['verify']
-    valid_email = request.form['email']
+    username = request.form['username']
+    password = request.form['password']
+    verify = request.form['verify']
+    email = request.form['email']
 
     username_error = ''
     password_error = ''
     verify_error = ''
     email_error = ''
 
-    if int(len(valid_username)) <= 0:
+    if int(len(username)) <= 0:
         username_error = 'Thats not a valid username'
-        valid_username = ''
+        username = ''
     else:
-        if int(len(valid_username)) < 3 or int(len(valid_username)) > 20:
+        if int(len(username)) < 3 or int(len(username)) > 20:
             username_error = 'Thats not a valid username'
-            valid_username = ''
+            username = ''
     
-    if int(len(valid_password)) <= 0:
+    if int(len(password)) <= 0:
         password_error = 'Thats not a valid password'
-        valid_password = ''
+        password = ''
     else:
-        if int(len(valid_password)) < 3 or int(len(valid_password)) > 20:
+        if int(len(password)) < 3 or int(len(password)) > 20:
             password_error = 'Thats not a valid password'
-            valid_password = ''
+            password = ''
 
-    if int(len(valid_verify)) <= 0:
+    if int(len(verify)) <= 0:
         verify_error = 'Password do not match'
-        valid_verify = ''
+        verify = ''
     else:
-        if valid_password != valid_verify:
+        if password != verify:
             verify_error = 'Password do not match'
-            valid_verify = ''  
+            verify = ''  
     
-    if int(len(valid_email)) > 0:
-        if "@" not in valid_email and "." not in valid_email and " " not in valid_email:
+    if int(len(email)) > 0:
+        if "@" not in email and "." not in email and " " not in email:
             email_error = 'Thats not a valid email'
             email = ''
-        elif int(len(valid_email)) < 3 or int(len(valid_password)) > 20:
+        elif int(len(email)) < 3 or int(len(password)) > 20:
             email_error = 'Thats not a valid email'
             email = ''
 
     if not username_error and not password_error and not verify_error and not email_error:
-        valid_username = str(valid_username)
-        return redirect('/welcome_page?username={0}'.format(valid_username))
+        username = str(username)
+        return redirect('/welcome_page?username={0}'.format(username))
     else:
-        return render_template('homepage.html', username_error=username_error, password_error=password_error, 
-        verify_error=verify_error, email_error=email_error, valid_username=valid_username, valid_password=valid_password, valid_verify=valid_verify, valid_email=valid_email)
+        return render_template('index.html', username_error=username_error, password_error=password_error, 
+        verify_error=verify_error, email_error=email_error, username=username, password=password, verify=verify, email=email)
 
 @app.route("/welcome_page")
 def welcome():
-    valid_username = request.args.get('username')
-    return render_template('welcome_page.html', valid_username = valid_username)
+    username = request.args.get('username')
+    return render_template('welcome_page.html', username = username)
 
 app.run()
